69c0e16a779aca76fea64717023aca3de33989f3
abhisheky1817 <abhisheky.1817@gmail.com>
2026-09-02 15:43:28 +0530

    add repo data access layer

---
+++ backend/src/repositories/repositoryData.js
@@ -0,0 +1,23 @@
+import prisma from '../lib/prismaClient.js';
+
+export async function createRepository(data) {
+  return prisma.repository.create({
+    data,
+  });
+}
+
+export async function findRepositoryByGithubId(githubId) {
+  return prisma.repository.findUnique({
+    where: {
+      githubId,
+    },
+  });
+}
+
+export async function findRepositoryById(id) {
+  return prisma.repository.findUnique({
+    where: {
+      id,
+    },
+  });
+}
